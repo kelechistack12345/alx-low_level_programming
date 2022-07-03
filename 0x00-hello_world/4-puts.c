1b534f8d1aabdc4b36486024555fc2c1c5c81107
kelechistack12345 <unachukwukelechi7@gmail.com>
2022-07-02 18:34:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *Description:'Write a C program that prints exact
+ *
+ *    *Return:Always 0 (Success)
+ *
+ *     */
+
+
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+
+
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+
+
+	return (0);
+
+
+
 }
+
+
+
+
+
+
 
