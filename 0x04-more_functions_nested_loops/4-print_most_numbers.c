4265048094952060a3af51e089408aeb9afd1975
kelechistack12345 <unachukwukelechi7@gmail.com>
2022-07-08 08:41:22 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,18 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
-       	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+	  	{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
