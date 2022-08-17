678f3f57d5b32562a6fc850a8579fcece79a20e6
kelechistack12345 <unachukwukelechi7@gmail.com>
2022-08-16 21:48:51 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * listint_len - the number of elements in a linked listint_t list.
+ *
+ * @h: head pointer
+ *
+ * Return: the number of elements
+ *
  */
-size_t list_len(const list_t *h)
-{
-	int count = 0;
 
-	while (h)
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
 	{
+	h = h->next;
 		count++;
-		h = h->next;
 	}
 	return (count);
 }
